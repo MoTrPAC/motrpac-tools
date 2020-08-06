804f101633adb442a95edcc2eb64729a1ec98796
Steven Hershman <hershman@Stevens-MacBook-Pro.local>
2020-08-06 15:09:42 -0700

    pep8 fixes

--- make_manifest.py
+++ make_manifest.py
@@ -20,15 +20,19 @@ def MoTrPACManifest(basepath, outfile, sep=','):
             lines += 1
     o.close()
     if lines == 0:
-        raise Exception("No files in basepath or basepath not found. Please double check")
+        raise Exception("No files in basepath or basepath not found. "
+                        "Please double check")
     else:
         print("wrote data for %d files" % lines)
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Creates manifest for submission to BIC: a comma separated file table of relative file paths and md5 sums.')
+    description = 'Creates manifest for submission to BIC: a comma ' \
+                  'separated file table of relative file paths and md5 sums.'
+    parser = argparse.ArgumentParser(description=description)
     parser.add_argument('data_path',
-                        help='Full path to folder containing all files for data submission')
+                        help='Full path to folder containing all files '
+                             'for data submission')
     parser.add_argument('output', default='manifest.txt',
                         help='Path to the output file')
     args = parser.parse_args()
