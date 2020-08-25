2ab024158ed8212c007c45b97104dc91e875bb3c
alchemistmatt <matthew.monroe@pnnl.gov>
2020-08-24 17:14:35 -0700

    Display relative file paths as they are processed
    Use -q or --quiet to silently process files

--- make_manifest.py
+++ make_manifest.py
@@ -3,8 +3,9 @@ import os
 import argparse
 
 
-def MoTrPACManifest(basepath, outfile, sep=','):
+def MoTrPACManifest(basepath, outfile, quiet=False, sep=','):
     BLOCKSIZE = 65536
+
     # Determine full paths
     outfilepath_full = os.path.abspath(outfile)
     basepath_full = os.path.abspath(basepath)
@@ -28,6 +29,9 @@ def MoTrPACManifest(basepath, outfile, sep=','):
             if os.path.sep != '/':
                 relativepath = relativepath.replace(os.path.sep, '/')
                 
+            if not quiet:
+                print(relativepath)
+
             hasher = hashlib.md5()
             with open(str(fp), 'rb') as afile:
                 buf = afile.read(BLOCKSIZE)
@@ -48,15 +52,17 @@ def MoTrPACManifest(basepath, outfile, sep=','):
 
 def main():
     description = 'Creates manifest for submission to BIC: a comma ' \
-                  'separated file table of relative file paths and md5 sums.'
+                  'separated file with relative file paths and md5 sums.'
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('data_path',
                         help='Full path to folder containing all files '
                              'for data submission')
-    parser.add_argument('output', default='manifest.txt',
+    parser.add_argument('output', default='file_manifest.csv',
                         help='Path to the output file')
+    parser.add_argument('-q', '--quiet', action='store_true',
+                        help='When provided, do not show file paths as they are hashed.')
     args = parser.parse_args()
-    MoTrPACManifest(args.data_path, args.output)
+    MoTrPACManifest(args.data_path, args.output, args.quiet)
 
 
 if __name__ == "__main__":
